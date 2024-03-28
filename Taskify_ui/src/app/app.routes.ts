448a58dc0dc5d16046f9c2fe14befa53776f8e8b
Amanuel <Amanuel.Eshete1@gmail.com>
2024-03-28 10:52:01 +0300

    Adding header component with menu button.

--- Taskify_ui/src/app/app.routes.ts
+++ Taskify_ui/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
-import { UserComponent } from './component/user/user.component';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
-    {path: 'user', component:UserComponent}
+    {path: 'user', loadComponent: () => import('./component/user/user.component').then(m=> m.UserComponent)}
 ];
---
+++ Taskify_ui/src/app/component/menuheader/menuheader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menuheader',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './menuheader.component.html',
+  styleUrl: './menuheader.component.css'
+})
+export class MenuheaderComponent {
+
+}
