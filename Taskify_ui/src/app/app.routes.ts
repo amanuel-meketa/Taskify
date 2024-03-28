a95d1ae5a0961f1da6e8441e35c2e8728076cf26
Amanuel <Amanuel.Eshete1@gmail.com>
2024-03-28 10:18:06 +0300

    Add home and user components

--- Taskify_ui/src/app/app.routes.ts
+++ Taskify_ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { UserComponent } from './component/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'user', component:UserComponent}
+];
